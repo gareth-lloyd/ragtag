d0e887b247bd69c0932f3d03cd7c6d3b52425f0c
Gareth Lloyd <glloyd@glloyd-laptop.(none)>
2010-11-02 00:07:05 +0000

    remove unnecessary comments from urls.py

--- urls.py
+++ urls.py
@@ -4,14 +4,4 @@ from django.conf.urls.defaults import *
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Example:
-    # (r'^ragtag/', include('ragtag.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = patterns('',)
