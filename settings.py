37d1d12198d4c3f3d9418bc123aefe98bf7a84c0
Gareth Lloyd <glloyd@glloyd-laptop.(none)>
2010-11-07 17:34:37 -0500

    update templates and urls to permit customization of the basic/blog themes

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 # Django settings for ragtag project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,12 +12,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ragtag',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -78,9 +79,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'ragtag.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
 )
 
 INSTALLED_APPS = (
@@ -89,6 +88,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
+    'django.contrib.comments',
+    'django.contrib.markup',
+    'basic.blog',
+    'basic.inlines',
+    'tagging'
 )
--- urls.py
+++ urls.py
@@ -1,7 +1,33 @@
 from django.conf.urls.defaults import *
+from basic.blog import views
+import settings
+from django.contrib import admin
+admin.autodiscover()
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+# patterns for contrib modules
+urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
+    (r'^comments/', include('django.contrib.comments.urls')),
+)
+# blog urls
+urlpatterns += patterns('',
+    url(r'^$', view=views.post_list, name='post_list'),
+    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$',
+        view=views.post_detail,
+        name='blog_detail'
+    ),
+    url(r'^categories/(?P<slug>[-\w]+)/$', view=views.category_detail,
+        name='blog_category_detail'
+    ),
+    url (r'^categories/$', view=views.category_list, name='blog_category_list'),
+    url(r'^tags/(?P<slug>[-\w]+)/$', view=views.tag_detail,
+        name='blog_tag_detail'
+    ),
+)
 
-urlpatterns = patterns('',)
+
+# serve static assets using the django's inbuilt method if in debug
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^assets/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/glloyd/projects/ragtag/assets'}),
+    )
